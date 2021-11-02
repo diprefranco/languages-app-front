87d7a6844c2213c388004e7fe77ddba37932aeb3
Franco DIPRE <DipreF@sancristobal.com.ar>
2021-11-02 09:15:33 -0300

    LayoutModule with header, footer and sidebar.

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { MatListModule } from '@angular/material/list'; 
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatListModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class LayoutModule { }
