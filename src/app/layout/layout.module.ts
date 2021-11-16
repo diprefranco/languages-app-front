c3c8bedef453133e60ef090126820cd935aad6d8
Franco DIPRE <DipreF@sancristobal.com.ar>
2021-11-16 18:58:44 -0300

    New lesson: basic form added.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 import { LessonsRoutingModule } from './lessons-routing.module';
 import { NewLessonComponent } from './components/new-lesson/new-lesson.component';
-
 
 @NgModule({
   declarations: [
@@ -11,6 +17,12 @@ import { NewLessonComponent } from './components/new-lesson/new-lesson.component
   ],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
     LessonsRoutingModule
   ]
 })
