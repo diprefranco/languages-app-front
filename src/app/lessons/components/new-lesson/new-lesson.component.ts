cb803f9daead649c5f27590b2c6fe2f53a6dd479
Franco DIPRE <DipreF@sancristobal.com.ar>
2021-12-02 18:13:22 -0300

    New lesson: focus on word input field, validate form, clean input and focus it after submit.

--- src/app/lessons/components/new-lesson/new-lesson.component.ts
+++ src/app/lessons/components/new-lesson/new-lesson.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, ElementRef, OnInit, AfterViewChecked, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-lesson',
   templateUrl: './new-lesson.component.html',
   styleUrls: ['./new-lesson.component.css']
 })
-export class NewLessonComponent implements OnInit {
+export class NewLessonComponent implements OnInit, AfterViewChecked {
   wordForm: FormGroup;
   wordsLearnt: string[] = [];
+  @ViewChild('wordInput') wordInputElement: ElementRef;
+
+  constructor(
+    private cdRef: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     this.setWordForm();
   }
 
+  ngAfterViewChecked() {
+    this.wordInputElement.nativeElement.focus();
+    this.cdRef.detectChanges();
+  }
+
   get wordFormInput() { return this.wordForm.get('wordFormInput') }
   private setWordForm() {
     this.wordForm = new FormGroup({
-      wordFormInput: new FormControl('')
+      wordFormInput: new FormControl('', Validators.required)
     });
   }
 
   wordFormSubmit() {
-    this.wordsLearnt.push(this.wordFormInput?.value);
+    if (!this.wordForm.valid) return;
+    this.wordsLearnt.push(`${this.wordFormInput?.value}`.trim());
+    this.wordForm.reset();
+    this.wordInputElement.nativeElement.focus();
   }
-
 }
